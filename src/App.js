d87c74efbb28cb56e37974ef94ea0d9fbb995978
datsomiro <61497396+datsomiro@users.noreply.github.com>
2021-03-01 13:37:46 +0100

    table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Data from './Components/Table.js';
+import Table from './Components/Table.js';
 import data from './Constants/data.json';
 
 function App() {
 
   const { close, offexchtradevolumeeex, onexchtradevolumeeex, tradedatetimegmt } = data //destructuring
+
   return (
     <div className="App">
         <h1>Test Limpide</h1>
